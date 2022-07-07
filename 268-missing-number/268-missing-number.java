697be437452c1fef7b90850ffa331e5ef0834380
Naseem <86598705+Naseemmm@users.noreply.github.com>
2022-07-07 13:49:33 +0530

    Time: 2 ms (31.63%), Space: 50.3 MB (81.10%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,27 +1,26 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        List<Integer> a=new ArrayList<>();
-        
-        int i=0;
-    while(i<nums.length)
-    {
-        int c=nums[i];
-        if(nums[i]<nums.length&&nums[i]!=nums[c])
+      
+       int i=0;
+        while(i<nums.length)
         {
-            int temp=nums[i];
-            nums[i]=nums[c];
-            nums[c]=temp;
+            int j=nums[i];
+            if(nums[i]<nums.length&&nums[i]!=nums[j])
+            {
+                int t=nums[i];
+                nums[i]=nums[j];
+                nums[j]=t;
+            }
+            else
+                i++;
         }
-        else
-        i++;
-    }
-       for(int j=0;j<nums.length;j++)
-       {
-           if(nums[j]!=j)
-           {
-               return j;
-           }
-       }
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]!=j)
+            {
+                return j;
+            }
+        }
        return nums.length;
     }
 }
