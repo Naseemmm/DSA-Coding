54b7e5e97da173c91378a247caa62705bf8fbf15
Naseem <86598705+Naseemmm@users.noreply.github.com>
2022-07-09 13:25:24 +0530

    Time: 2 ms (17.55%), Space: 42.3 MB (31.96%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String toLowerCase(String s) {
+         String z="";
+        for(int i=0;i<s.length();i++){
+        z=s.toLowerCase();
+        }
+        return z;
+    }
+}
