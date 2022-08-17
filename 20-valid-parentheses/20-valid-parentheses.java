670872621838b4fd5dac03f55bcbdb38908163e8
Naseem <86598705+Naseemmm@users.noreply.github.com>
2022-08-17 23:58:02 +0530

    Time: 2 ms (90.88%), Space: 41.8 MB (75.66%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,39 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-   if(s.length()%2!=0)
-        {
-            return false;
-        }
-        Stack <Character> st=new Stack<>();
+        Stack<Character> st=new Stack<>();
         for(char c:s.toCharArray())
         {
-            if(c=='[' || c=='{' || c=='(')
+            if(c=='{' || c=='['||c=='(')
             {
                 st.push(c);
             }
-            else {
+            else
+            {
                 if(st.isEmpty())
-                {
                     return false;
-                }
-                if(c==']'&& st.peek()!='[')
-            {
-               return false;
-            }
-            if(c=='}'&&st.peek()!='{')
-            {
-                return false;
-            }
-              if(c==')' &&st.peek()!='(')
-            {
-               return false;
-            } 
+                if(c=='}' && !st.isEmpty() && st.peek()!='{')
+                    return false;
+                if(c==']' && !st.isEmpty() && st.peek()!='[')
+                    return false;
+                if(c==')' && !st.isEmpty() && st.peek()!='(')
+                    return false;
                 else
                     st.pop();
             }
-            
-                
         }
         return st.isEmpty();
     }
-};
+}
