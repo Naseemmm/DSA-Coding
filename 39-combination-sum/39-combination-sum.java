def99e40542ccb9e245837d4c29faf46baa06c80
Naseem <86598705+Naseemmm@users.noreply.github.com>
2022-08-21 13:12:58 +0530

    Time: 3 ms (95.48%), Space: 45.7 MB (37.35%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,26 +1,29 @@
 class Solution {
-    void fun(int ind,int target,int[] candidates,List<List<Integer>>ans,List<Integer>ds)
+    void fun(int ind,int target,int[] candidates,List<Integer>ds,List<List<Integer>>ans)
     {
         if(ind==candidates.length)
         {
             if(target==0)
-            {
-                ans.add(new ArrayList<Integer>(ds));
-                    
-            }
-            return;
+                ans.add(new ArrayList<>(ds));
+            return ;
         }
         if(candidates[ind]<=target)
         {
             ds.add(candidates[ind]);
-            fun(ind,target-candidates[ind],candidates,ans,ds);
+            fun(ind,target-candidates[ind],candidates,ds,ans);
             ds.remove(ds.size()-1);
         }
-        fun(ind+1,target,candidates,ans,ds);
+        fun(ind+1,target,candidates,ds,ans);
+        
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
-        fun(0,target,candidates,ans,new ArrayList<>());
-        return ans;
+        fun(0,target,candidates,new ArrayList<>(),ans);
+        List<List<Integer>> op = new ArrayList<>();
+    for(List a:ans)
+    {
+        op.add(a);
+    }
+   return op;
     }
 }
