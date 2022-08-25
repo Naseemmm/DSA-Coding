48ea629cf847a26159a7ad07cd401cd66f9a986a
Naseem <86598705+Naseemmm@users.noreply.github.com>
2022-08-26 00:19:17 +0530

    Time: 2 ms (86.33%), Space: 43.9 MB (68.36%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,19 +1,21 @@
 class Solution {
-    void fun(int ind,int[] nums,ArrayList<Integer>ds,List<List<Integer>>ans)
+    void fun(int ind,int[] nums,List<Integer>ds,List<List<Integer>>ans)
     {
-        ans.add(new ArrayList<>(ds));
+       
+        
+           ans.add(new ArrayList<>(ds)); 
+        
         for(int i=ind;i<nums.length;i++)
         {
-            if( i!=ind && nums[i]==nums[i-1]) continue;
+            if(i>ind && nums[i]==nums[i-1])continue;
             ds.add(nums[i]);
             fun(i+1,nums,ds,ans);
             ds.remove(ds.size()-1);
-            
         }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
          List<List<Integer>>ans=new ArrayList<>();
+        Arrays.sort(nums);
         fun(0,nums,new ArrayList<>(),ans);
         return ans;
     }
