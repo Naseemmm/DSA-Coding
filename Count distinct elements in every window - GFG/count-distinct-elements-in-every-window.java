128ea5a6e867670f26f257abaf64dba2122ab852
Naseem <86598705+Naseemmm@users.noreply.github.com>
2022-10-18 20:14:03 +0530

    Added solution - LeetHub

--- Count distinct elements in every window - GFG/count-distinct-elements-in-every-window.java
+++ Count distinct elements in every window - GFG/count-distinct-elements-in-every-window.java
@@ -38,25 +38,19 @@ class Solution
         HashMap<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<k;i++)
         {
-            if(map.containsKey(A[i]))
-               map.put(A[i],map.get(A[i])+1);
-             else
-               map.put(A[i],1);
+            map.put(A[i],map.getOrDefault(A[i],0)+1);
         }
         a.add(map.size());
         for(int i=k;i<n;i++)
         {
-                if(map.get(A[i-k])==1)
-                  map.remove(A[i-k]);
-                else
-                   {
-                       map.put(A[i-k],map.get(A[i-k])-1);
-                   }
-                   if(map.containsKey(A[i]))
-                   map.put(A[i],map.get(A[i])+1);
-                   else
-                     map.put(A[i],1);
-             a.add(map.size());  
+            if(map.get(A[i-k])==1)
+              map.remove(A[i-k]);
+               else
+               {
+                   map.put(A[i-k],map.getOrDefault(A[i-k],0)-1);
+               }
+             map.put(A[i],map.getOrDefault(A[i],0)+1);
+             a.add(map.size());
         }
         return a;
     }
